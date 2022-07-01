b937ba06442726c0c8f27ff58fea554b0ab002f8
Fazalullah Jafar <107798016+whilooper@users.noreply.github.com>
2022-07-01 10:20:15 +0400

    Create itunes-search.py

---
+++ itunes-search.py
@@ -0,0 +1,11 @@
+import json
+# Make sure you have installed requests using --> pip install requests
+import requests
+artist= input("What do you want to search? ")
+response = requests.get("https://itunes.apple.com/search?entity=song&limit=50&term="+artist)
+
+
+o = response.json()
+
+for result in o["results"]:
+    print("-->",result["trackName"])
