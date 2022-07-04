54630b56e08a99e9c23b09747cc9b32e294506eb
Fazalullah Jafar <107798016+whilooper@users.noreply.github.com>
2022-07-04 11:17:51 +0400

    Improved itunes browser

--- itunes-search.py
+++ itunes-search.py
@@ -1,6 +1,8 @@
 import json
 # Make sure you have installed requests using --> pip install requests
 import requests
+print("""       Itunes Browser      
+Using this program, you can search for tracks in the Itunes...""")
 artist= input("What do you want to search? ")
 response = requests.get("https://itunes.apple.com/search?entity=song&limit=50&term="+artist)
 
